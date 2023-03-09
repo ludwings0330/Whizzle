783d4f40445ccd945a740e9deae0525dd057e890
이다운 <innocent61@naver.com>
2023-03-10 08:49:50 +0900

    :bug: Fix: 컨트롤러에서 상태 코드를 입력하지 않았을 때 발생하는 버그 해결

--- back/src/main/java/com/bear/whizzle/common/aop/LogAspect.java
+++ back/src/main/java/com/bear/whizzle/common/aop/LogAspect.java
@@ -1,6 +1,7 @@
 package com.bear.whizzle.common.aop;
 
 import java.lang.reflect.Parameter;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -13,6 +14,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -65,10 +67,14 @@ public class LogAspect {
     @AfterReturning(value = "controller()", returning = "dto")
     public void afterReturningController(JoinPoint joinPoint, Object dto) {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        ResponseStatus responseStatus = signature.getMethod()
-                                                 .getDeclaredAnnotation(ResponseStatus.class);
+        HttpStatus httpStatus = Optional.ofNullable(
+                                                signature.getMethod()
+                                                         .getDeclaredAnnotation(ResponseStatus.class)
+                                        )
+                                        .map(ResponseStatus::value)
+                                        .orElse(HttpStatus.OK);
 
-        log.info(RESPONSE_LOG, responseStatus.value(), dto);
+        log.info(RESPONSE_LOG, httpStatus, dto);
         log.info(FINISH_LINE);
     }
 
