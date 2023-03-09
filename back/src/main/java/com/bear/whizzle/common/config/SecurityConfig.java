a439da2e6397513e9023a922af82fd9efad0251d
ludwings0330 <ludwings0330@gmail.com>
2023-03-09 14:58:02 +0900

    :sparkles: Feat: OAuth 2.0 관련 Security Config 추가
    - Disable CORS
    - OAuth2 Config 추가

--- back/src/main/java/com/bear/whizzle/common/config/SecurityConfig.java
+++ back/src/main/java/com/bear/whizzle/common/config/SecurityConfig.java
@@ -1,22 +1,42 @@
 package com.bear.whizzle.common.config;
 
+import com.bear.whizzle.auth.service.CustomOAuth2UserService;
+import com.bear.whizzle.common.handler.CustomAuthenticationFailureHandler;
+import com.bear.whizzle.common.handler.CustomAuthenticationSuccessHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.cors().disable()
-                   .authorizeRequests(
-                           requests -> requests.anyRequest().permitAll()
-                   )
-                   .httpBasic(Customizer.withDefaults())
-                   .build();
+
+        http.oauth2Login(oauth2 ->
+                                 oauth2
+                                         .userInfoEndpoint()
+                                         .userService(customOAuth2UserService)
+                                         .and()
+                                         .successHandler(customAuthenticationSuccessHandler)
+                                         .failureHandler(customAuthenticationFailureHandler));
+
+        http.authorizeRequests(request ->
+                                       request.antMatchers("/login/**", "/redirect/**")
+                                              .permitAll()
+                                              .anyRequest()
+                                              .authenticated());
+
+        http.csrf().disable();
+
+        return http.build();
     }
 
 }
