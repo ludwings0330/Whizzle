13a44779beef2e804fcf0c96a433ca77abc67596
ludwings0330 <ludwings0330@gmail.com>
2023-03-09 15:12:17 +0900

    :bug: Fix: Entity 에 id 제공을 위해 Sequence table 을 사용하던 문제 수정

--- back/src/main/java/com/bear/whizzle/domain/model/entity/Token.java
+++ back/src/main/java/com/bear/whizzle/domain/model/entity/Token.java
@@ -3,6 +3,7 @@ package com.bear.whizzle.domain.model.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -23,7 +24,7 @@ import lombok.ToString;
 public class Token {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "member_id")
