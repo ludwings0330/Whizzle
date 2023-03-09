29e69b5a938fdc8b684ebed7325383f51d23e5d1
이다운 <innocent61@naver.com>
2023-03-09 17:13:58 +0900

    :sparkles: Feat: S3 테스트를 위한 메서드 구현
    - 추후에 실제 구현에 참고할 것
    - 다중 파일 업로드는 따로 구현해야 함

---
+++ back/src/main/java/com/bear/whizzle/cloud/aws/s3/service/AwsS3Service.java
@@ -0,0 +1,65 @@
+package com.bear.whizzle.cloud.aws.s3.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.bear.whizzle.domain.model.type.Image;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class AwsS3Service {
+
+    private final AmazonS3 amazonS3;
+    private final String bucketName;
+
+    @Autowired
+    public AwsS3Service(AmazonS3 amazonS3, @Value("${cloud.aws.s3.bucket}") String bucketName) {
+        this.amazonS3 = amazonS3;
+        this.bucketName = bucketName;
+    }
+
+    public Image upload(MultipartFile multipartFile, String dirName) {
+        String originalName = multipartFile.getOriginalFilename();
+        String key = key(dirName, originalName);
+        ObjectMetadata metadata = getObjectMetadata(multipartFile);
+
+        try (InputStream input = multipartFile.getInputStream()) {
+            amazonS3.putObject(
+                    new PutObjectRequest(
+                            this.bucketName, key, input, metadata
+                    ).withCannedAcl(CannedAccessControlList.PublicRead)
+            );
+        } catch (IOException e) {
+            throw new IllegalArgumentException("업로드 하려는 파일을 읽을 수 없습니다.", e);
+        }
+
+        return Image.builder()
+                    .originalName(originalName)
+                    .url(amazonS3.getUrl(this.bucketName, key))
+                    .build();
+    }
+
+    public void remove(String dirName, String originalName) {
+        String key = key(dirName, originalName);
+        amazonS3.deleteObject(this.bucketName, key);
+    }
+
+    private String key(String dirName, String originalName) {
+        return dirName + "/" + UUID.randomUUID() + "_" + originalName;
+    }
+
+    private ObjectMetadata getObjectMetadata(MultipartFile multipartFile) {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(multipartFile.getContentType());
+        metadata.setContentLength(multipartFile.getSize());
+        return metadata;
+    }
+
+}
