d9e35db2532433bffac38469f4e8d7ed17b6c3b5
ludwings0330 <ludwings0330@gmail.com>
2023-03-09 12:52:16 +0900

    :sparkles: Feat: PrincipalDetails 구현

--- back/src/main/java/com/bear/whizzle/auth/service/PrincipalDetails.java
+++ back/src/main/java/com/bear/whizzle/auth/service/PrincipalDetails.java
@@ -1,5 +1,106 @@
 package com.bear.whizzle.auth.service;
 
-public class PrincipalDetails {
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class PrincipalDetails implements OAuth2User, UserDetails {
+
+    private String email;
+    private String nickname;
+    private String provider;
+
+    public static PrincipalDetails of(String provider, Map<String, Object> attributes) {
+        switch (provider) {
+            case "GOOGLE":
+                return ofGoogle(attributes);
+            case "NAVER":
+                return ofNaver(attributes);
+            case "KAKAO":
+                return ofKakao(attributes);
+            default:
+                return null;
+        }
+    }
+
+    private static PrincipalDetails ofKakao(Map<String, Object> attributes) {
+        return null;
+    }
+
+    private static PrincipalDetails ofNaver(Map<String, Object> attributes) {
+        return null;
+    }
+
+    private static PrincipalDetails ofGoogle(Map<String, Object> attributes) {
+        return PrincipalDetails.builder()
+                               .provider("GOOGLE")
+                               .email((String) attributes.get("email"))
+                               .nickname((String) attributes.get("name"))
+                               .build();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.nickname;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getName() {
+        return this.email;
+    }
+
+    @Override
+    public <A> A getAttribute(String name) {
+        return OAuth2User.super.getAttribute(name);
+    }
 
 }
