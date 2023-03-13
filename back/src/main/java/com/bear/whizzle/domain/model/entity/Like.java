8b9331d8f6b30aa4627e3fd15352ad1e46fb22d3
이다운 <innocent61@naver.com>
2023-03-13 14:25:38 +0900

    :hammer: Refactor: Like 클래스에서 ID 수정
    - 복합키 사용을 위해 기존의 ID 필드 제거
    - 복합키 사용을 위한 ID 클래스 정의
    - 복합키 적용

--- back/src/main/java/com/bear/whizzle/domain/model/entity/Like.java
+++ back/src/main/java/com/bear/whizzle/domain/model/entity/Like.java
@@ -1,57 +1,49 @@
 package com.bear.whizzle.domain.model.entity;
 
+import com.bear.whizzle.domain.model.type.LikeId;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 
 @Entity
-@Table(
-        name = "likes",
-        uniqueConstraints = @UniqueConstraint(columnNames = { "member_id", "review_id" })
-)
-
+@Table(name = "likes")
+@IdClass(LikeId.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @Builder
-@ToString
 public class Like {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", updatable = false)
-    @NotNull
-    @ToString.Exclude
-    private Member member;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "review_id", updatable = false)
-    @NotNull
-    @ToString.Exclude
+    @JoinColumn(name = "review_id")
     private Review review;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     @CreatedDate
     @Column(columnDefinition = "DATETIME", updatable = false)
     @NotNull
     private LocalDateTime createdDateTime;
+
+    public String toString() {
+        return "Like [review.id: " + review.getId() + ", member.id: " + member.getId() + "]";
+    }
 
 }
