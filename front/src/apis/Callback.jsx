098e59ff13862aff3c64167fa03d6baaeff80428
zxzx9404 <zxzx9404@gmail.com>
2023-03-13 16:35:59 +0900

    :bug: Fix: login Callback funcion 로직 수정
    Former-commit-id: eb7dec0cb130ad46f6b5af8b7bf20d7913c222b5

--- front/src/apis/Callback.jsx
+++ front/src/apis/Callback.jsx
@@ -1,50 +1,34 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 const Callback = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const queryParams = new URLSearchParams(location.search);
-  const [isStored, setIsStored] = useState(false);
 
   const accessToken = queryParams.get("accessToken");
   // access 토큰의 payload에서 유저 정보 추출 후, 전역 변수로 저장 필요
   const refreshToken = queryParams.get("refreshToken");
-  const isNew = Boolean(queryParams.get("isNew"));
-
-  console.log(queryParams);
-
-  function settingSuccess() {
-    setIsStored(true);
-    console.log("세팅 성공");
-  }
-
-  function settingFail() {
-    setIsStored(true);
-    console.log("세팅 실패");
-  }
+  const isNew = queryParams.get("isNew") === "true";
 
   function setting() {
     try {
       sessionStorage.setItem("accessToken", accessToken);
       sessionStorage.setItem("refreshToken", refreshToken);
-      settingSuccess();
     } catch (error) {
-      settingFail();
+      console.log("Token 저장 실패");
     }
   }
 
-  setting();
-
   useEffect(() => {
-    if (isStored) {
-      if (isNew) {
-        navigate("/recommend/question");
-      } else {
-        navigate("/");
-      }
+    setting();
+
+    if (isNew) {
+      navigate("/recommend/question");
+    } else {
+      navigate("/");
     }
-  }, [isStored, navigate, isNew]);
+  }, []);
 
   return null;
 };
