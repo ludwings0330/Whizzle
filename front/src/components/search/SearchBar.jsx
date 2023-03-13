84789b2722644f9f2de1f3aa5a92666863e3111b
zxzx9404 <zxzx9404@gmail.com>
2023-03-13 17:52:41 +0900

    :sparkles: Feat: 검색페이지 기본 기능 구현 완료
    Former-commit-id: ced0258a1d5c0927dc0945b706a83111c52f057a

--- front/src/components/search/SearchBar.jsx
+++ front/src/components/search/SearchBar.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const SInput = styled.input`
+  width: 862px;
+  height: 69px;
+  border: 1px solid #c1c1c1;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 999px;
+  padding-left: 50px;
+  font-size: 24px;
+
+  ::placeholder {
+    font-size: 24px;
+    color: #adadad;
+    text-indent: 10px;
+  }
+`;
 
 //검색 창
 const SearchBar = () => {
+  const [searchWord, setSearchWord] = useState("");
+
+  const wordChange = (e) => {
+    setSearchWord(e.target.value);
+    console.log(searchWord);
+  };
   return (
     <>
-      <h1>검색창</h1>
+      <SInput placeholder="찾고자 하는 위스키 이름을 입력하세요." onChange={wordChange} />
     </>
   );
 };
