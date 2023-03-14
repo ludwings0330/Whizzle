745f0f762d8536245ee6a35e8f9418f4d0586fc4
zxzx9404 <zxzx9404@gmail.com>
2023-03-14 09:43:26 +0900

    :sparkles: Feat: 로그인 시 JWT에서 유저 정보 추출 로직 추가
    Former-commit-id: 6f617763f438d2c2a256a15d8ef9faaba3d0d29a

--- front/src/apis/Callback.jsx
+++ front/src/apis/Callback.jsx
@@ -1,13 +1,16 @@
 import { useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { userState } from "../store/userStore";
+import jwtDecode from "jwt-decode";
 
 const Callback = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const queryParams = new URLSearchParams(location.search);
+  const setUser = useSetRecoilState(userState);
 
   const accessToken = queryParams.get("accessToken");
-  // access 토큰의 payload에서 유저 정보 추출 후, 전역 변수로 저장 필요
   const refreshToken = queryParams.get("refreshToken");
   const isNew = queryParams.get("isNew") === "true";
 
@@ -15,6 +18,16 @@ const Callback = () => {
     try {
       sessionStorage.setItem("accessToken", accessToken);
       sessionStorage.setItem("refreshToken", refreshToken);
+
+      // access 토큰의 payload에서 유저 정보 추출 후, 전역 변수로 저장
+      const jwt = jwtDecode(accessToken);
+      setUser((prev) => ({
+        ...prev,
+        nickname: jwt.nickname,
+        email: jwt.email,
+        provider: jwt.provider,
+        exp: jwt.exp,
+      }));
     } catch (error) {
       console.log("Token 저장 실패");
     }
