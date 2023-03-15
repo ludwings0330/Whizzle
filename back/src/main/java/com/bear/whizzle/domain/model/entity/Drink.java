b7f29f72cbd54895609795f49dac3e346a7c6fb7
이다운 <innocent61@naver.com>
2023-03-15 23:04:58 +0900

    :hammer: Refactor: Drink 클래스에 Order와 isDeleted 필드 추가
    Former-commit-id: 19239470674d7ed8077a476c0193d3a1d11375df

--- back/src/main/java/com/bear/whizzle/domain/model/entity/Drink.java
+++ back/src/main/java/com/bear/whizzle/domain/model/entity/Drink.java
@@ -1,6 +1,7 @@
 package com.bear.whizzle.domain.model.entity;
 
 import com.bear.whizzle.domain.model.type.id.DrinkId;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
@@ -8,19 +9,18 @@ import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Table(name = "drink")
 @IdClass(DrinkId.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
-@Builder
 public class Drink {
 
     @Id
@@ -32,6 +32,21 @@ public class Drink {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "whisky_id", updatable = false)
     private Whisky whisky;
+
+    @Column(columnDefinition = "TINYINT")
+    @NotNull
+    private Integer whiskyOrder;
+
+    @NotNull
+    @ColumnDefault("0")
+    private Boolean isDeleted = Boolean.FALSE;
+
+    @Builder
+    public Drink(Diary diary, Whisky whisky, Integer whiskyOrder) {
+        this.diary = diary;
+        this.whisky = whisky;
+        this.whiskyOrder = whiskyOrder;
+    }
 
     public String toString() {
         return "Drink [diary.id: " + diary.getId() + ", whisky.id: " + whisky.getId() + "]";
