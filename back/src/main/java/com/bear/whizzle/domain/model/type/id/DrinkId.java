144aed260d36a88903e0978308a37701481f7b63
이다운 <innocent61@naver.com>
2023-03-15 14:53:59 +0900

    :hammer: Refactor: 필요한 Lombok 애노테이션만 사용하도록 수정
    Former-commit-id: b71f0ddb6f2357d0abb38c777402ebd0fed2fb8a

--- back/src/main/java/com/bear/whizzle/domain/model/type/id/DrinkId.java
+++ back/src/main/java/com/bear/whizzle/domain/model/type/id/DrinkId.java
@@ -1,24 +1,16 @@
 package com.bear.whizzle.domain.model.type.id;
 
 import java.io.Serializable;
-import javax.persistence.Embeddable;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Getter
-@Builder
 @EqualsAndHashCode
 public class DrinkId implements Serializable {
 
     private Long diary;
 
-    private Long whiskey;
+    private Long whisky;
 
 }
--- back/src/main/java/com/bear/whizzle/domain/model/type/id/LikeId.java
+++ back/src/main/java/com/bear/whizzle/domain/model/type/id/LikeId.java
@@ -1,19 +1,11 @@
 package com.bear.whizzle.domain.model.type.id;
 
 import java.io.Serializable;
-import javax.persistence.Embeddable;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Getter
-@Builder
 @EqualsAndHashCode
 public class LikeId implements Serializable {
 
