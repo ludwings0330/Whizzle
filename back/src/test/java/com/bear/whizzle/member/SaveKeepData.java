6118188301169686684662be4d5398ac424284fb
이다운 <innocent61@naver.com>
2023-03-17 17:48:57 +0900

    :sparkles: Feat: DB에 Keep 데이터 추가
    Former-commit-id: 17f3b037739c705df734e78d047e4219a2cbe2ac

---
+++ back/src/test/java/com/bear/whizzle/member/SaveKeepData.java
@@ -0,0 +1,62 @@
+package com.bear.whizzle.member;
+
+import static com.bear.whizzle.common.util.RandomDataUtil.MEMBER_SIZE;
+
+import com.bear.whizzle.common.util.RandomDataUtil;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootTest
+@Slf4j
+class SaveKeepData {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public SaveKeepData(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Test
+    void saveKeepData() {
+        List<List<Long>> keepIds = new ArrayList<>();
+        final int BATCH_SIZE = 100_000;
+
+        for (long memberId = 1; memberId <= MEMBER_SIZE; memberId++) {
+            int keepCount = (int) (100 * Math.random());
+            Set<Long> whiskyIds = RandomDataUtil.getWhiskeyIds(keepCount);
+
+            for (Long whiskyId : whiskyIds) {
+                keepIds.add(List.of(memberId, whiskyId));
+
+                if (keepIds.size() == BATCH_SIZE) {
+                    bulkInsert(keepIds);
+                    keepIds.clear();
+                }
+            }
+        }
+
+        if (!keepIds.isEmpty()) {
+            bulkInsert(keepIds);
+        }
+    }
+
+    private void bulkInsert(List<List<Long>> keepIds) {
+        final String INSERT_SQL = "INSERT INTO keep (member_id, whisky_id, created_date_time) VALUES (?, ?, now())";
+        jdbcTemplate.batchUpdate(
+                INSERT_SQL, keepIds, keepIds.size(),
+                (preparedStatement, keepId) -> {
+                    int idx = 0;
+                    preparedStatement.setLong(++idx, keepId.get(0));
+                    preparedStatement.setLong(++idx, keepId.get(1));
+                }
+        );
+    }
+
+}
