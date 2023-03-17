e80ba296d78afbbd5c68d22e6be49cb0be4855ee
ludwings0330 <ludwings0330@gmail.com>
2023-03-18 00:19:29 +0900

    :hammer: Refactor: 유저 기본 정호 조회 API 스펙 수정
    - email, provider 를 url 에 노출시키지 않도록 수정
    
    
    Former-commit-id: 585ce4390ed49f80d0e2e761988750449013fe94

--- back/src/main/java/com/bear/whizzle/member/controller/MemberController.java
+++ back/src/main/java/com/bear/whizzle/member/controller/MemberController.java
@@ -2,11 +2,12 @@ package com.bear.whizzle.member.controller;
 
 import com.bear.whizzle.domain.model.entity.Member;
 import com.bear.whizzle.member.controller.dto.MemberBaseInfoDto;
+import com.bear.whizzle.member.controller.dto.MemberBaseInfoRequestDto;
 import com.bear.whizzle.member.converter.MemberConverter;
 import com.bear.whizzle.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +18,12 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/email/{email}/provider/{provider}")
-    public MemberBaseInfoDto findMember(@PathVariable String email, @PathVariable String provider) {
-        final Member member = memberService.findByEmailAndProvider(email, provider);
+    @GetMapping
+    public MemberBaseInfoDto findMember(@RequestBody MemberBaseInfoRequestDto requestDto) {
+        final Member member = memberService.findByEmailAndProvider(requestDto.getEmail(), requestDto.getProvider());
 
         return MemberConverter.toMemberBaseInfoDto(member);
     }
+
 
 }
---
+++ back/src/main/java/com/bear/whizzle/member/controller/dto/MemberBaseInfoRequestDto.java
@@ -0,0 +1,11 @@
+package com.bear.whizzle.member.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberBaseInfoRequestDto {
+
+    private String email;
+    private String provider;
+
+}
