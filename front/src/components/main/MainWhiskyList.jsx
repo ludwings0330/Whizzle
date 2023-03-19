4e44d67f9b042e96aa667db9597d6d5519e8e0a5
yejin <yejinlee0707@gmail.com>
2023-03-20 00:16:26 +0900

    :lipstick: Design: 메인, 푸터 임시 디자인 추가
    Former-commit-id: 102e5d1a09fdae9987fe4468ba375c37e86c57a4

--- front/src/components/main/MainWhiskyList.jsx
+++ front/src/components/main/MainWhiskyList.jsx
@@ -2,11 +2,7 @@ import React from "react";
 
 //메인 화면에 띄워줄 추천 위스키 리스트(로그인 사용자)
 const MainWhiskyList = () => {
-  return (
-    <>
-      <h1>로그인 사용자에게 위스키 추천</h1>
-    </>
-  );
+  return <>{/* <h1>로그인 사용자에게 위스키 추천</h1> */}</>;
 };
 
 export default MainWhiskyList;
