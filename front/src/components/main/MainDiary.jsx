4c9ad51f5b5a300ef738a3ae4488ed6b76c08bfb
unknown <yejinlee0707@gmail.com>
2023-03-20 16:08:28 +0900

    :lipstick: Design: 메인페이지 UI 디자인 추가
    Former-commit-id: 2e8812f922f86c771a049f12af4350735cad5822
    Former-commit-id: 9ded3b7b927c061badd44bde5dc31cfb55ed9080

--- front/src/components/main/MainDiary.jsx
+++ front/src/components/main/MainDiary.jsx
@@ -1,18 +1,61 @@
 import React from "react";
 import styled from "styled-components";
-import main2 from "../../assets/img/main2.png";
+import { motion } from "framer-motion";
+import diaryImg from "../../assets/img/main_diary.png";
 
-const SImg = styled.div`
+const SDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SContent = styled.div`
+  width: 60%;
   height: 100vh;
-  background-image: url(${main2});
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const SLight = styled.span`
+  font-family: GmarketSansLight;
+  font-size: 40px;
+  padding-left: 10px;
+`;
+
+const SStrong = styled.span`
+  font-family: GmarketSansBold;
+  font-size: 40px;
+  padding-left: 10px;
 `;
 
 //메인화면에 띄워줄 다이어리
-const MainDiary = () => {
+const MainDiary = (props) => {
   return (
-    <>
-      <SImg />
-    </>
+    <SDiv>
+      <SContent>
+        <motion.div
+          variants={props.variants}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+        >
+          <SLight>위스키 한잔에 담아보는 오늘의 기억</SLight>
+          <br />
+          <SStrong>
+            <span style={{ color: "#F84F5A" }}>기억을 기록하다,</span> 위스키 다이어리
+          </SStrong>
+        </motion.div>
+        <motion.div
+          variants={props.variants}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+        >
+          <img src={diaryImg} alt="diary.png" />
+        </motion.div>
+      </SContent>
+    </SDiv>
   );
 };
 
--- front/src/pages/AppMain.jsx
+++ front/src/pages/AppMain.jsx
@@ -1,27 +1,52 @@
 import React from "react";
-import styled from "styled-components";
-import main from "../assets/img/main.png";
+import { useRef } from "react";
+import { motion } from "framer-motion";
 
 //components import
+import Main from "../components/main/Main";
 import MainDiary from "../components/main/MainDiary";
-import MainRecommend from "../components/main/MainRecommend";
 import MainReview from "../components/main/MainReview";
+import MainRecommend from "../components/main/MainRecommend";
 import MainWhiskyList from "../components/main/MainWhiskyList";
 
-const SImg = styled.div`
-  height: 100vh;
-  background-image: url(${main});
-`;
-
 const AppMain = () => {
+  const scrollRef = useRef(null);
+  const variants = {
+    hidden: { opacity: 0, y: 100 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      rotateY: 0,
+      transition: {
+        rotateY: {
+          duration: 0.3,
+        },
+        y: {
+          type: "spring",
+          stiffness: 50,
+          restDelta: 0.01,
+          duration: 0.3,
+        },
+      },
+    },
+  };
+
   return (
-    <>
-      <SImg />
-      <MainDiary />
-      <MainRecommend />
-      <MainReview />
-      <MainWhiskyList />
-    </>
+    <div ref={scrollRef}>
+      <Main key="0" variants={variants} viewport={{ root: scrollRef, once: true, amount: 0.3 }} />
+      <MainDiary
+        key="1"
+        variants={variants}
+        viewport={{ root: scrollRef, once: true, amount: 0.3 }}
+      />
+      <MainReview
+        key="2"
+        variants={variants}
+        viewport={{ root: scrollRef, once: true, amount: 0.3 }}
+      />
+      {/* <MainRecommend />
+      <MainWhiskyList /> */}
+    </div>
   );
 };
 
