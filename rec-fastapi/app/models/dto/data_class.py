62cabceb7a27b3526b2cfa9171c3a6c6d4f38516
JunHyxxn <ihrug000@gmail.com>
2023-03-22 11:00:44 +0900

    :sparkles: Feat: data_class 파일 정의
    - Preference DTO 정의
    
    
    Former-commit-id: db0d2b8a6354c093b5b0a9b48493134d786a0eec
    Former-commit-id: ce32eb7ddd17e323acf9f3ad41f07c4bc6f41207

---
+++ rec-fastapi/app/models/dto/data_class.py
@@ -0,0 +1,24 @@
+from fastapi import Body, Query
+from pydantic import BaseModel
+
+
+class Flavor(BaseModel):
+    smoky: float = Body(..., ge=0, le=1)
+    peaty: float = Body(..., ge=0, le=1)
+    spicy: float = Body(..., ge=0, le=1)
+    herbal: float = Body(..., ge=0, le=1)
+    oily: float = Body(..., ge=0, le=1)
+    body: float = Body(..., ge=0, le=1)
+    rich: float = Body(..., ge=0, le=1)
+    sweet: float = Body(..., ge=0, le=1)
+    salty: float = Body(..., ge=0, le=1)
+    vanilla: float = Body(..., ge=0, le=1)
+    tart: float = Body(..., ge=0, le=1)
+    fruity: float = Body(..., ge=0, le=1)
+    floral: float = Body(..., ge=0, le=1)
+
+
+class Preference(BaseModel):
+    user_id: int = Body(..., alias="userId")
+    price_tier: int = Body(..., alias="priceTier", ge=0, le=5)
+    flavor: Flavor
