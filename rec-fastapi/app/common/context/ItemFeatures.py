9f784ef532c7a68e73cc48bae457fe865dd874bd
JunHyxxn <ihrug000@gmail.com>
2023-03-22 16:56:23 +0900

    :sparkles: Feat: item_features application 실행 시 load하도록 변경
    - item_features의 경우 불변 데이터로 매번 불러올 필요가 없습니다.
    - 따라서, application 시작 시 메모리에 올려두고 사용하도록 변경합니다.
    
    
    Former-commit-id: 921a6fe5ce1b97d7d76cc649b818058337cc2f20
    Former-commit-id: 2923b3eb1d12b97da3b1e5307f9421884207c8da

---
+++ rec-fastapi/app/common/context/ItemFeatures.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from scipy.sparse import csr_matrix
+from common.config import settings
+
+
+class ItemFeatures:
+    __instance = None
+    data = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    def __init__(self):
+        if self.data == None:
+            self.load_item_features()
+
+    def load_item_features(self):
+        self.data = pd.read_csv(
+            settings.ITEM_FEATURES_PATH, index_col=0, encoding=settings.ENCODING)
+        self.data = csr_matrix(self.data)
