c5f6d6a11ee792dbefd5f1ee36a3be5527065eba
JunHyxxn <ihrug000@gmail.com>
2023-03-22 12:47:07 +0900

    :sparkles: Feat: 모델 재학습 controller 구현
    Former-commit-id: 89de27ec7b8ddc8e1021823ca327c7475ec90676
    Former-commit-id: ba3ec4d9693f017abcb9755cf6ca0d5ed707d2f6

--- rec-fastapi/app/routers/recommend.py
+++ rec-fastapi/app/routers/recommend.py
@@ -13,6 +13,15 @@ rec = APIRouter(
 )
 
 
+async def retrain_model():
+    time.sleep(5)
+    print("after sleep")
+    if random.randint(0, 1) == 0:
+        return "zero"
+    else:
+        return "one"
+
+
 @rec.get("/personal-whisky", status_code=200)
 async def rec_personal_whisky(preference: Preference = Body(...)):
     return predict_personal_whisky(preference)
@@ -21,3 +30,10 @@ async def rec_personal_whisky(preference: Preference = Body(...)):
 @rec.get("/similary-whisky/{whisky_id}", status_code=200)
 async def rec_similar_whisky(whisky_id: int = Path(..., ge=0, le=3534)):
     return predict_similar_whisky(whisky_id)
+
+
+@rec.get("/async/", status_code=202)
+async def rec_async(background_tasks: BackgroundTasks):
+    a = 100
+    print(a)
+    return background_tasks.add_task(retrain_model)
