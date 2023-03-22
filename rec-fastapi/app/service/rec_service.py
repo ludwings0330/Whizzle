a149644ef07e3afb945071b2f6f8e67e5281f509
JunHyxxn <ihrug000@gmail.com>
2023-03-22 17:04:46 +0900

    :sparkles: Feat: item_features 최초 load 후 사용하도록 변경
    - main.py : app.on_event("startup") 을 통해 application 시작 시 load 메소드 수행
    - rec_service.py, recommend.py : 로직 수정
    
    
    Former-commit-id: 78beeeaae0a4d6596286ec8d5a71101e57a49199
    Former-commit-id: a3f64e0d38f7248ad6a4c33840e4b839f7bfba31

--- rec-fastapi/app/service/rec_service.py
+++ rec-fastapi/app/service/rec_service.py
@@ -7,6 +7,7 @@ from scipy.sparse import csr_matrix
 import lightfm as LightFM
 
 from models.dto.data_class import Preference
+from common.config import settings
 
 
 def load_rec_model():
@@ -14,21 +15,21 @@ def load_rec_model():
     return model
 
 
-def load_item_features():
-    item_features = pd.read_csv(
-        "./models/save/item_features.csv", index_col=0, encoding="UTF-8"
-    )
-    return csr_matrix(item_features)
+# memory에 띄워서 관리
+# def load_item_features():
+#     item_features = pd.read_csv(
+#         "./models/save/item_features.csv", index_col=0, encoding="UTF-8"
+#     )
+#     return csr_matrix(item_features)
 
 
 def make_user_features(preference: Preference):
     return csr_matrix([preference.price_tier] + list(vars(preference.flavor).values()))
 
 
-def predict_personal_whisky(preference: Preference):
+def predict_personal_whisky(preference: Preference, item_features):
     model = load_rec_model()
     user_features = make_user_features(preference)
-    item_features = load_item_features()
     item_ids = np.arange(item_features.shape[0])
     scores = model.predict(
         user_ids=preference.user_id,
@@ -39,8 +40,7 @@ def predict_personal_whisky(preference: Preference):
     return np.argsort(-scores).tolist()
 
 
-def predict_similar_whisky(whisky_id: int, k: int = 5):
-    item_features = load_item_features()
+def predict_similar_whisky(whisky_id: int, item_features, k: int = 5):
     # get Cosine Similarity
     cosine_sim = cosine_similarity(item_features, item_features)
 
