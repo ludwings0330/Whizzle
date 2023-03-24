a5cf74715a96de381b045aa8a0e3aa1e7b5b4803
JunHyxxn <ihrug000@gmail.com>
2023-03-24 16:57:45 +0900

    :sparkles: Feat: 맛 Min, Max 조회, 위스키별 Price Tier 조회
    ---
    - 조회의 경우 cache에 data가 있다면 캐시에서 바로 찾아오고
    - 캐시에 없다면 DB에서 조회합니다.
    - @Cacheable를 이용하게 되면 실행 시점에 안전하게 캐시에 저장되지 않는 문제가 발생해서 Annotation이 아닌 직접 넣고, 수정하도록 구현했습니다.
    
    
    Former-commit-id: ae42bc7f4d5a539f0f05f25d3ccfa90e15b6361d
    Former-commit-id: 6197dd4c8990733cdc3efff17cc466522f9789fa

--- back/src/main/java/com/bear/whizzle/whisky/service/query/WhiskyQueryService.java
+++ back/src/main/java/com/bear/whizzle/whisky/service/query/WhiskyQueryService.java
@@ -1,5 +1,12 @@
 package com.bear.whizzle.whisky.service.query;
 
+import com.bear.whizzle.whisky.repository.projection.dto.FlavorSummary;
+import java.util.Map;
+
 public interface WhiskyQueryService {
+
+    FlavorSummary findFlavorMinMax();
+
+    Map<Long, Integer> findWhiskyPriceTier();
 
 }
