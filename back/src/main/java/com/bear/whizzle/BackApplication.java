e6ef7d795f35629dd404ed441038a3ce477f097a
JunHyxxn <ihrug000@gmail.com>
2023-03-24 16:53:28 +0900

    :sparkles: Feat: 로컬 캐시 설정 파일 작성
    Former-commit-id: ba5d4967b94ecad5328645f05eb868610923da48
    Former-commit-id: ac0267182989aaf268d298f47c41cbb14f688381

--- back/src/main/java/com/bear/whizzle/BackApplication.java
+++ back/src/main/java/com/bear/whizzle/BackApplication.java
@@ -5,12 +5,14 @@ import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableCaching
 public class BackApplication {
 
     public static void main(String[] args) {
