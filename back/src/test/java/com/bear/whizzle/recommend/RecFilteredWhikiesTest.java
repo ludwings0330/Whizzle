ba31050b320ef5454dac24bb54629acc47baee5b
JunHyxxn <ihrug000@gmail.com>
2023-03-27 13:11:54 +0900

    :umbrella: Test: 추천 위스키 가격대 필터링 테스트
    Former-commit-id: 16b9ea6ae2668189f98e3fb36c630d6f0f0bc8cf
    Former-commit-id: 45b1545163a383ce6cb561d90da44fb27cabca1e

---
+++ back/src/test/java/com/bear/whizzle/recommend/RecFilteredWhikiesTest.java
@@ -0,0 +1,54 @@
+package com.bear.whizzle.recommend;
+
+import com.bear.whizzle.domain.model.entity.Whisky;
+import com.bear.whizzle.recommend.service.RecService;
+import com.bear.whizzle.whisky.repository.WhiskyRepository;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecFilteredWhikiesTest {
+
+    @Autowired
+    private RecService recService;
+    @Autowired
+    private WhiskyRepository whiskyRepository;
+
+    @Value("${app.rec.topK}")
+    private Integer topK;
+
+    @Test
+    @DisplayName("가격대 필터링 테스트")
+    void filteredWhiskiesTest() {
+        // given
+        // Create a list of Long integers from 1 to 3535
+        List<Long> numbers = new ArrayList<>();
+        for (long i = 1; i <= 3535; i++) {
+            numbers.add(i);
+        }
+
+        // Shuffle the list randomly
+        Collections.shuffle(numbers);
+        Integer priceTier = 4;
+
+        // when
+        List<Long> filteredWhiskies = recService.filterByPriceTier(numbers, priceTier);
+        List<Whisky> whiskies = new ArrayList<>(whiskyRepository.findByIds(filteredWhiskies).values());
+
+        // then
+        Assertions.assertThat(filteredWhiskies).hasSize(topK);
+        whiskies.forEach(w -> {
+            Assertions.assertThat(w.getPriceTier()).isEqualTo(priceTier);
+        });
+    }
+
+}
