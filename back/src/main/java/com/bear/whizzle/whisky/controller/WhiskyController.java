63d0550b943e81b12b6397a656777cc4d6f16a52
이다운 <innocent61@naver.com>
2023-03-27 13:09:19 +0900

    :sparkles: Feat: 위스키 상세 정보 조회 기능 구현
    Former-commit-id: db3255dea7c27b748ccc5393df729f15a0061deb
    Former-commit-id: 90b6157c79378ff37189025651f755c60b7a9c42

--- back/src/main/java/com/bear/whizzle/whisky/controller/WhiskyController.java
+++ back/src/main/java/com/bear/whizzle/whisky/controller/WhiskyController.java
@@ -1,39 +1,36 @@
 package com.bear.whizzle.whisky.controller;
 
+import com.bear.whizzle.whisky.controller.dto.WhiskyDetailResponseDto;
+import com.bear.whizzle.whisky.mapper.WhiskyMapper;
 import com.bear.whizzle.whisky.service.WhiskyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/whiskies")
 @RequiredArgsConstructor
 public class WhiskyController {
 
     private final WhiskyService whiskyService;
 
-    @GetMapping
+    /**
+     * 위스키 상세 정보 조회에 성공할 경우 그 정보와 상태 코드 200으로 응답
+     *
+     * @param whiskyId 조회하려는 위스키 ID
+     * @return 위스키 상세 정보
+     * @throws com.bear.whizzle.domain.exception.NotFoundException 잘못된 위스키 ID로 조회하여 위스키를 찾을 수 없을 때 발생
+     */
+    @GetMapping("/{whiskyId}")
     @ResponseStatus(HttpStatus.OK)
-    public String test() {
-        return whiskyService.test();
+    public WhiskyDetailResponseDto findWhisky(@PathVariable Long whiskyId) {
+        return WhiskyMapper.toWhiskyDetailResponseDto(
+                whiskyService.findWhisky(whiskyId)
+        );
     }
-
-    @GetMapping("/exception")
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String testException() {
-        try {
-            return whiskyService.testException();
-        } catch (Exception e) {
-            throw new RuntimeException("다른 예외로 래핑", e);
-        }
-    }
-
-    @GetMapping("/sleep")
-    @ResponseStatus(HttpStatus.OK)
-    public String sleep() throws InterruptedException {
-        return whiskyService.sleep();
-    }
-
 
 }
