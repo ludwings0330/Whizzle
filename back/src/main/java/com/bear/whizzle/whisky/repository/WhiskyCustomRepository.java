54b57f25425e86d77ac70cb2fa6e5db5d2f72382
JunHyxxn <ihrug000@gmail.com>
2023-03-27 09:40:03 +0900

    :sparkles: Feat: index 리스트로 위스키 정보 조회
    - 순서 정렬을 위해 Map 으로 반환합니다
    
    
    Former-commit-id: 7f44f8f468d96ac1b3cb818d497196ac2363b440
    Former-commit-id: 3d6d373d3d6c08610599b2fce3b32dbbebe21b62

--- back/src/main/java/com/bear/whizzle/whisky/repository/WhiskyCustomRepository.java
+++ back/src/main/java/com/bear/whizzle/whisky/repository/WhiskyCustomRepository.java
@@ -1,9 +1,13 @@
 package com.bear.whizzle.whisky.repository;
 
 import com.bear.whizzle.domain.model.entity.Whisky;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface WhiskyCustomRepository {
+
+    Map<Long, Whisky> findByIds(List<Long> ids);
 
     Optional<Whisky> findByIdAfterSleepTest(Long id);
 
--- back/src/main/java/com/bear/whizzle/whisky/repository/WhiskyCustomRepositoryImpl.java
+++ back/src/main/java/com/bear/whizzle/whisky/repository/WhiskyCustomRepositoryImpl.java
@@ -3,9 +3,11 @@ package com.bear.whizzle.whisky.repository;
 import static com.bear.whizzle.domain.model.entity.QWhisky.*;
 
 import com.bear.whizzle.common.annotation.Performance;
-import com.bear.whizzle.domain.model.entity.QWhisky;
 import com.bear.whizzle.domain.model.entity.Whisky;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -15,6 +17,16 @@ import org.springframework.stereotype.Repository;
 public class WhiskyCustomRepositoryImpl implements WhiskyCustomRepository {
 
     private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Map<Long, Whisky> findByIds(List<Long> ids) {
+        Map<Long, Whisky> whiskies = new HashMap<>();
+        queryFactory.selectFrom(whisky)
+                    .where(whisky.id.in(ids))
+                    .fetch()
+                    .forEach(w -> whiskies.put(w.getId(), w));
+        return whiskies;
+    }
 
     @Override
     @Performance
