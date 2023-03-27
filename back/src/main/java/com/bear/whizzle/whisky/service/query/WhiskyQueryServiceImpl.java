12bfad7a71eae2b277dbe2e5bd35de6c3ff18a2e
JunHyxxn <ihrug000@gmail.com>
2023-03-27 10:18:29 +0900

    :poop: Fix: String 직접 사용하지 않도록 수정
    Former-commit-id: 36a779fccc0de09ac199cfd7d16adf22f7995c22
    Former-commit-id: 2c555671e2a999ca97bea20176c1467e0510d227

--- back/src/main/java/com/bear/whizzle/whisky/service/query/WhiskyQueryServiceImpl.java
+++ back/src/main/java/com/bear/whizzle/whisky/service/query/WhiskyQueryServiceImpl.java
@@ -33,7 +33,7 @@ public class WhiskyQueryServiceImpl implements WhiskyQueryService {
         Cache flavorCache = cacheManager.getCache(CacheType.FLAVOR_MINMAX.getCacheName());
         FlavorSummary summary = flavorCache.get(flavorKey, FlavorSummary.class);
         if (summary == null) {
-            flavorCache.put("flavorMinMax", whiskyProjectionRepository.findFlavorMinMax());
+            flavorCache.put(flavorKey, whiskyProjectionRepository.findFlavorMinMax());
         }
         return summary;
     }
@@ -44,7 +44,7 @@ public class WhiskyQueryServiceImpl implements WhiskyQueryService {
         Cache priceCache = cacheManager.getCache(CacheType.WHISKY_PRICE_TIER.getCacheName());
         Map<Long, Integer> priceTierMap = priceCache.get(priceKey, Map.class);
         if (priceTierMap == null) {
-            priceCache.put("whiskyPriceTier", whiskyRepository.findAll()
+            priceCache.put(priceKey, whiskyRepository.findAll()
                                                               .stream()
                                                               .collect(Collectors.toMap(Whisky::getId, Whisky::getPriceTier)));
         }
