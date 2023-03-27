dfd9f4b88a6e1aef6fd506c28e7daf13735e15ba
이다운 <innocent61@naver.com>
2023-03-27 16:42:41 +0900

    :umbrella: Test: 위스키 킵을 취소하는 경우의 테스트
    Former-commit-id: ddae336dba3d165df8dd75dcd235c60a65fc6a94
    Former-commit-id: 7188ecdbf65ee3bb172f4c3feab6774ac7bb9a3a

--- back/src/test/java/com/bear/whizzle/keep/controller/KeepControllerTest.java
+++ back/src/test/java/com/bear/whizzle/keep/controller/KeepControllerTest.java
@@ -1,13 +1,17 @@
 package com.bear.whizzle.keep.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.bear.whizzle.auth.service.PrincipalDetails;
 import com.bear.whizzle.common.util.JwtUtil;
 import com.bear.whizzle.domain.exception.NotFoundException;
+import com.bear.whizzle.domain.model.entity.Keep;
 import com.bear.whizzle.keep.repository.KeepRepository;
+import java.util.Optional;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,24 +34,30 @@ class KeepControllerTest {
     private KeepRepository keepRepository;
 
     @Test
-    @DisplayName("위스키 킵")
-    void keepWhisky() throws Exception {
+    @DisplayName("위스키 킵 & 취소 테스트")
+    void keepAndUnkeepWhisky() throws Exception {
         // given
         final Long TEST_MEMBER_ID = 1L;
         final String TEST_MEMBER_TOKEN = jwtUtil.generateToken(PrincipalDetails.builder().memberId(TEST_MEMBER_ID).build(), 5000);
         final Long TEST_WHISKY_ID = 1L;
 
-        // when
+        // when 1
         mockMvc.perform(
                 post("/api/keeps/" + TEST_WHISKY_ID)
                         .header("Authorization", "Bearer " + TEST_MEMBER_TOKEN)
         ).andExpect(status().isOk());
 
-        // then
-        assertThatCode(
-                () -> keepRepository.findByMemberIdAndWhiskyId(TEST_MEMBER_ID, TEST_WHISKY_ID)
-                                    .orElseThrow(() -> new NotFoundException("위스키 킵을 실패했습니다."))
-        ).doesNotThrowAnyException();
+        // then 1
+        assertThat(keepRepository.findByMemberIdAndWhiskyId(TEST_MEMBER_ID, TEST_WHISKY_ID)).isPresent();
+
+        // when 2
+        mockMvc.perform(
+                post("/api/keeps/" + TEST_WHISKY_ID)
+                        .header("Authorization", "Bearer " + TEST_MEMBER_TOKEN)
+        ).andExpect(status().isOk());
+
+        // then 2
+        assertThat(keepRepository.findByMemberIdAndWhiskyId(TEST_MEMBER_ID, TEST_WHISKY_ID)).isEmpty();
     }
 
 }
