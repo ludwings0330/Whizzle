6c6abd85f6b8082b1afbee9aa70ff01db9de76f1
ludwings0330 <ludwings0330@gmail.com>
2023-03-28 23:33:24 +0900

    :hammer: Refactor: 민감 정보 숨김
    elastic search Origin 숨김 처리
    
    
    Former-commit-id: 9b9fe82254173d766708a19c7e4028ff5c826111
    Former-commit-id: 697dc5c2186e59bef2881cb1568e3f1a27f90abc

--- back/src/main/java/com/bear/whizzle/common/config/ElasticSearchConfig.java
+++ back/src/main/java/com/bear/whizzle/common/config/ElasticSearchConfig.java
@@ -1,6 +1,7 @@
 package com.bear.whizzle.common.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
@@ -11,10 +12,13 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
+    @Value("${spring.elasticsearch.url}")
+    private String url;
+
     @Override
     public RestHighLevelClient elasticsearchClient() {
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                                                                     .connectedTo("localhost:9200")
+                                                                     .connectedTo(url)
                                                                      .build();
 
         return RestClients.create(clientConfiguration).rest();
