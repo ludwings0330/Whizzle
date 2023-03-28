1b89357b5bf806651673921d3e4c062d339b7678
zxzx9404 <zxzx9404@gmail.com>
2023-03-28 10:07:37 +0900

    :sparkles: Feat: 메인 추천 위스키 preset 작성 중
    Former-commit-id: 1451aef3ee31aa9d1d25be031cacd7f50bd44d1f
    Former-commit-id: 0c07c73b63e5adb3b50994c216ff079f082a25ab

---
+++ front/src/constants/presetWhisky.js
@@ -0,0 +1,160 @@
+const presetWhisky = [
+  {},
+  {
+    id: 2243,
+    name: "글렌피딕 12년",
+    posterNumber: 1,
+  },
+  {
+    id: 2474,
+    name: "글렌리벳 12년",
+    posterNumber: 2,
+  },
+  {
+    id: 1360,
+    name: "맥켈란 12년",
+    posterNumber: 3,
+  },
+  {
+    id: 2477,
+    name: "글렌모렌지 10년",
+    posterNumber: 4,
+  },
+  {
+    id: 1977,
+    name: "발베니 더블우드",
+    posterNumber: 5,
+  },
+  {
+    id: 95,
+    name: "라프로익 10년",
+    posterNumber: 6,
+  },
+  {
+    id: 1859,
+    name: "글렌 그란트 12년",
+    posterNumber: 7,
+  },
+  {
+    id: 816,
+    name: "보모어 12년",
+    posterNumber: 8,
+  },
+  {
+    id: 2469,
+    name: "잭 다니엘 No.7",
+    posterNumber: 9,
+  },
+  {
+    id: 3293,
+    name: "짐 빔 오리지널",
+    posterNumber: 10,
+  },
+  {
+    id: 3350,
+    name: "제임슨",
+    posterNumber: 11,
+  },
+  {
+    id: 1562,
+    name: "산토리 토키",
+    posterNumber: 12,
+  },
+  {
+    id: 3007,
+    name: "와일드 터키 101",
+    posterNumber: 13,
+  },
+  {
+    id: 2861,
+    name: "버팔로 트레이스",
+    posterNumber: 14,
+  },
+  {
+    id: 1995,
+    name: "메이커스 마크",
+    posterNumber: 15,
+  },
+  {
+    id: 2680,
+    name: "조니워커 블랙 라벨",
+    posterNumber: 16,
+  },
+  {
+    id: 2606,
+    name: "발렌타인 파이니스트",
+    posterNumber: 17,
+  },
+  {
+    id: 3022,
+    name: "시바스 리갈 12년",
+    posterNumber: 18,
+  },
+  {
+    id: 93,
+    name: "탈리스커 10년",
+    posterNumber: 19,
+  },
+  {
+    id: 812,
+    name: "야마자키 12년",
+    posterNumber: 20,
+  },
+  {
+    id: 2674,
+    name: "불렛 라이",
+    posterNumber: 21,
+  },
+  {
+    id: 2679,
+    name: "놉 크릭",
+    posterNumber: 22,
+  },
+  {
+    id: 157,
+    name: "조니워커 그린 라벨",
+    posterNumber: 23,
+  },
+  {
+    id: 1509,
+    name: "네이키드 몰트",
+    posterNumber: 24,
+  },
+  {
+    id: 818,
+    name: "이글 레어",
+    posterNumber: 25,
+  },
+  {
+    id: 1098,
+    name: "아드벡 10년",
+    posterNumber: 26,
+  },
+  {
+    id: 2487,
+    name: "우드포드 리저브",
+    posterNumber: 27,
+  },
+  {
+    id: 2855,
+    name: "오반 14년",
+    posterNumber: 28,
+  },
+  {
+    id: 1664,
+    name: "아벨라워 아부나흐",
+    posterNumber: 29,
+  },
+  {
+    id: 2850,
+    name: "크라운 로얄 디럭스",
+    posterNumber: 30,
+  },
+  {
+    id: 753,
+    name: "라가불린 8년",
+    posterNumber: 31,
+  },
+];
+
+export default presetWhisky;
