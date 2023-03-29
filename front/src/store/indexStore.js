24120533fe7cb190df24ed5be9ad5e2ae25b3373
unknown <yejinlee0707@gmail.com>
2023-03-29 16:45:59 +0900

    :hammer: Refactor: 데일리 추천 정보 전역변수로 변경
    Former-commit-id: eac383395afa82d3c5cf0c276272d03bec4fc8b6
    Former-commit-id: 3b55cadfc42fec8b54f5b1333481fd1d1d7fcb66

--- front/src/store/indexStore.js
+++ front/src/store/indexStore.js
@@ -30,3 +30,25 @@ export const recommendResult = atom({
   key: "recommendResult",
   default: [],
 });
+
+export const dailyPreference = atom({
+  key: "dailyPreference",
+  default: {
+    price: "",
+    flavor: {
+      smoky: 0,
+      peaty: 0,
+      spicy: 0,
+      herbal: 0,
+      oily: 0,
+      body: 0,
+      rich: 0,
+      sweet: 0,
+      salty: 0,
+      vanilla: 0,
+      tart: 0,
+      fruity: 0,
+      floral: 0,
+    },
+  },
+});
