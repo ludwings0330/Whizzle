bc5b874094fb44e92f43652c430ccd31c0f22875
zxzx9404 <zxzx9404@gmail.com>
2023-03-29 12:52:25 +0900

    :bug: Fix: 로그인 주소 변경
    Former-commit-id: 369d607632194b4e6c82a78f696da3fa919f01d7
    Former-commit-id: b2551c7d7c25b4a60037be98de874b297360b96c

--- front/src/pages/AppWhisky.jsx
+++ front/src/pages/AppWhisky.jsx
@@ -188,7 +188,7 @@ const AppWhisky = () => {
       setIsKeep(!isKeep);
       keepToggle(id);
     } else if (window.confirm("로그인이 필요한 기능입니다.\n로그인 페이지로 이동하시겠습니까?")) {
-      navigate("/login");
+      navigate("/signin");
     }
   };
 
