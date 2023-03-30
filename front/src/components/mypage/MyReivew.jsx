396298afdcc61072c81c02a6133ee17fc6946876
unknown <yejinlee0707@gmail.com>
2023-03-30 12:41:52 +0900

    :sparkles: Feat: 리뷰 무한스크롤 구현중
    Former-commit-id: 383c584d3bc402b5246d480763b53edfcd85640e
    Former-commit-id: dc8164c93d93dd9a973f8d6c6bbe5135ef131643

--- front/src/components/mypage/MyReivew.jsx
+++ front/src/components/mypage/MyReivew.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { reviewApi } from "../../apis/mypage";
 import { useRecoilValue } from "recoil";
@@ -44,69 +44,63 @@ const SWarning = styled.div`
   font-size: 18px;
 `;
 
-// const reviews = [
-//   {
-//     whiskyName: "Hibiki 21 Year",
-//     rating: 3.0,
-//     content: "content",
-//     likeCount: 0,
-//     createdDateTime: "2023-03-23T00:00:09",
-//     reviewId: 448566,
-//   },
-//   {
-//     whiskyName:
-//       "Middle West Spirits Sherry Cask Finished Bourbon Middle West Spirits Sherry Cask Finished Bourbon",
-//     rating: 1.0,
-//     content: "아무말이나 만들어보고 있습니다.",
-//     likeCount: 0,
-//     createdDateTime: "2023-03-22T07:46:12",
-//     reviewId: 1,
-//   },
-//   {
-//     whiskyName: "Springbank 17 Year Madeira Wood",
-//     rating: 2.0,
-//     content:
-//       "이 위스키는 정말 맛있네요. 라고 말하는 리뷰 내용을 작성했는데 길이가 짧아서 더 늘려보려고 합니다. 하하하 즐겁네요.",
-//     likeCount: 0,
-//     createdDateTime: "2023-03-22T07:46:12",
-//     reviewId: 2,
-//   },
-//   {
-//     whiskyName: "Hirsch Small Batch Reserve Straight Bourbon",
-//     rating: 3.0,
-//     content:
-//       "이 위스키는 정말 맛있네요. 라고 말하는 리뷰 내용을 작성했는데 길이가 짧아서 더 늘려보려고 합니다. 이 위스키는 정말 맛있네요. 라고 말하는 리뷰 내용을 작성했는데 길이가 짧아서 더 늘려보려고 합니다. 하하하 이 위스키는 정말 맛있네요. 라고 말하는 리뷰 내용을 작성했는데 길이가 짧아서 더 늘려보려고 합니다. 이 위스키는 정말 맛있네요. 라고 말하는 리뷰 내용을 작성했는데 길이가 짧아서 더 늘려보려고 합니다. 하하하",
-//     likeCount: 0,
-//     createdDateTime: "2023-03-22T07:46:12",
-//     reviewId: 3,
-//   },
-// ];
-
 const MyReivew = () => {
   const user = useRecoilValue(userState);
   const id = user.id;
   const [reviews, setReviews] = useState([]);
-
-  const myReviewApi = async () => {
-    const params = {
-      baseId: reviews.length > 0 ? reviews[reviews.length - 1].reviewId : 0,
-      reviewOrder: "RECENT",
-    };
-
-    const myReviews = await reviewApi(id, params);
-    setReviews(myReviews);
-  };
+  const [lastId, setLastId] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const observerRef = useRef(null);
 
   useEffect(() => {
-    myReviewApi();
-  }, []);
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          myReviewApi();
+        }
+      },
+      {
+        rootMargin: "0px",
+        threshold: 1.0,
+      }
+    );
+
+    if (observerRef.current) {
+      observer.observe(observerRef.current);
+    }
+
+    return () => {
+      if (observerRef.current) {
+        observer.unobserve(observerRef.current);
+      }
+    };
+  }, [observerRef, lastId]);
+
+  const myReviewApi = async () => {
+    if (!isLoading) {
+      setIsLoading(true);
+      console.log(lastId);
+      const params = {
+        baseId: lastId,
+        reviewOrder: "RECENT",
+      };
+
+      const myReviews = await reviewApi(id, params);
+      setLastId(myReviews[myReviews.length - 1].reviewId);
+      console.log(myReviews);
+      setReviews((prev) => {
+        return [...prev, ...myReviews];
+      });
+      setIsLoading(false);
+    }
+  };
 
   return (
     <>
       <SListDiv>
         {reviews.length > 0 ? (
-          reviews.map((review) => {
-            return <MyReviewItem key={review.reviewId} review={review} />;
+          reviews.map((review, index) => {
+            return <MyReviewItem key={index} review={review} />;
           })
         ) : (
           <SWarning>
@@ -114,6 +108,7 @@ const MyReivew = () => {
             <span>좋아하는 위스키에 대한 리뷰를 작성해보세요!</span>
           </SWarning>
         )}
+        <div ref={observerRef}></div>
       </SListDiv>
     </>
   );
