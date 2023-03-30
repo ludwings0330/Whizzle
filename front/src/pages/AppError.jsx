c079a50f828646b9adeca5e2e087ef27fda6072a
unknown <yejinlee0707@gmail.com>
2023-03-30 15:10:24 +0900

    :sparkles: Feat: 에러페이지 완성
    Former-commit-id: b531597cb758b90da332f2bbfd12a700ab1daa8e
    Former-commit-id: a79964a092bba16419b9e076499bf089d6a9e913

--- front/src/pages/AppError.jsx
+++ front/src/pages/AppError.jsx
@@ -1,13 +1,29 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
+import Lottie from "react-lottie";
+import animationData from "../assets/img/lotties/error-bear.json";
+// import animationData from "../assets/img/lotties/drinking-bear.json";
 import { changeHeader, rollbackHeader } from "../hooks/changeHeader";
 
 const SDiv = styled.div`
   display: flex;
+  margin-top: 70px;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   min-height: 100vh;
+`;
+
+const STitle = styled.div`
+  color: #181818;
+  font-weight: bold;
+  font-size: 100px;
+`;
+
+const SText = styled.div`
+  color: #181818;
+  // font-weight: bold;
+  font-size: 20px;
 `;
 
 const AppError = () => {
@@ -19,9 +35,21 @@ const AppError = () => {
     };
   }, []);
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
     <SDiv>
-      <h1>에러페이지</h1>
+      <STitle>404</STitle>
+      <SText>존재하지 않는 페이지입니다.</SText>
+      <SText style={{ marginBottom: "15px" }}>오신 김에 귀여운 곰돌이 한마리 보고 가세요.</SText>
+      <Lottie options={defaultOptions} height={600} width={600} />
     </SDiv>
   );
 };
