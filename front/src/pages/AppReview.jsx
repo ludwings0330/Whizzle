1a824d8d81c4b0e62d7ce231dfec139af3802c63
unknown <yejinlee0707@gmail.com>
2023-03-31 17:44:50 +0900

    :sparkles: Feat: 리뷰 작성 기능 구현 (진행중)
    Former-commit-id: 16db870a7dd1cb09511f98fdf7305acd0d99eb9a
    Former-commit-id: d1c03382b433b62633e0b18ee9afbdfa842c398e

--- front/src/pages/AppReview.jsx
+++ front/src/pages/AppReview.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { changeHeader, rollbackHeader } from "../hooks/changeHeader";
+import { whiskyDetail } from "../apis/whiskyDetail";
+import { reviewCreate } from "../apis/review";
 
 //import images
 import ImageUploader from "../components/review/create/ImageUploader";
@@ -9,11 +11,10 @@ import ImageUploader from "../components/review/create/ImageUploader";
 import ReviewContent from "../components/review/create/ReviewContent";
 import ReviewDetailInfo from "../components/review/create/ReviewDetailInfo";
 import ReviewRating from "../components/review/create/ReviewRating";
+import { useNavigate, useParams } from "react-router";
 
 const SContainer = styled.div`
-  margin-top: 150px;
-  max-width: 100%;
-  width: 100vw;
+  margin-top: 130px;
   display: flex;
   justify-content: center;
   flex-direction: column;
@@ -22,140 +23,151 @@ const SContainer = styled.div`
 `;
 
 const STitleDiv = styled.div`
-  width: 900px;
+  width: 830px;
   display: flex;
-  align-items: left;
+  justify-content: start;
+  align-items: center;
 `;
 
 const STitleP = styled.p`
   font-weight: bold;
-  font-size: 35px;
+  font-size: 32px;
+  color: #181818;
 `;
+
 const SP = styled.p`
-  font-size: 18px;
-  margin-top: 47px;
-  margin-left: 80px;
+  font-size: 16px;
+  margin-left: 70px;
+  color: #181818;
 `;
 
 const SButton = styled.button`
-  background-image: linear-gradient(
-    to right,
-    #f84f5a 0%,
-    #f84f5a 0%,
-    #f84f5a 21%,
-    #f2a660 52%,
-    #f2a660 78%,
-    #f2a660 100%
-  );
-  width: 280px;
-  height: 70px;
+  background-image: linear-gradient(90deg, #f84f5a 11.68%, #f2a660 86.99%);
+  width: 394px;
+  height: 74px;
   border-radius: 999px;
-  margin-top: 60px;
-  font-size: 22px;
-  font-weight: bold;
+  margin-top: 90px;
+  margin-bottom: 130px;
+  font-size: 18px;
+  font-family: Pretendard Variable;
   color: white;
   cursor: pointer;
   transition: all 0.3s ease-in-out;
   border: none;
-
-  &: hover {
-    background-image: linear-gradient(
-      to right,
-      #f2a660 0%,
-      #f2a660 0%,
-      #f2a660 21%,
-      #f84f5a 52%,
-      #f84f5a 78%,
-      #f84f5a 100%
-    );
-  }
 `;
 
 const SContentP = styled.p`
-  font-size: 25px;
+  font-size: 20px;
+  color: #181818;
   font-weight: bold;
-  margin-top: 50px;
+  margin-top: 70px;
 `;
 
 const SContentDiv = styled.div`
-  width: 900px;
+  width: 830px;
   display: flex;
   align-items: left;
 `;
 
 //리뷰작성 페이지
 const AppReview = () => {
-  const [images, setImages] = useState([]);
+  const navigate = useNavigate();
+  const [isCreate, setIsCreate] = useState();
+
   const [content, setContent] = useState("");
   const [rating, setRating] = useState(0);
 
+  const [whisky, setWhisky] = useState({});
+  const whiskyId = useParams().id;
+
+  const [preImages, setPreImages] = useState([]);
+  const [deleteImages, setDeleteImages] = useState([]);
+  const [files, setFiles] = useState([]);
+
+  const handlePreImages = (id) => {
+    setDeleteImages((prev) => [...prev, id]);
+    setPreImages(preImages.filter((image) => image.uid !== id));
+  };
+
+  const handleFiles = (datas) => {
+    setFiles(datas);
+  };
+
+  const reviewSave = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("whiskyId", whiskyId);
+    formData.append("rating", rating);
+    if (content !== "") {
+      formData.append("content", content);
+    }
+    console.log(whiskyId, rating, content, files);
+    if (files !== []) {
+      files.forEach((file) => {
+        formData.append("reviewImageFiles", file);
+      });
+    }
+
+    try {
+      await reviewCreate(formData);
+      navigate(`/review/${whiskyId}`);
+    } catch {
+      console.log("리뷰 저장 실패");
+    }
+  };
+
+  const getWhiskyInfo = async (id) => {
+    try {
+      const whiskyData = await whiskyDetail(whiskyId);
+      // console.log(whiskyData);
+      setWhisky(whiskyData);
+    } catch {
+      console.log("위스키 정보 조회 실패");
+    }
+  };
+
   // 페이지 mount시 네비게이션 바 이미지와 글씨 색 변경
   useEffect(() => {
+    getWhiskyInfo();
+
     changeHeader();
     return () => {
       rollbackHeader();
     };
   }, []);
 
-  useEffect(() => {
-    console.log(images);
-    console.log(content);
-    console.log(rating);
-  }, [images, rating, content]);
-
-  const exampleWhisky = {
-    id: 1,
-    name: "Glenfiddich 12 Year",
-    image: {
-      originalName: "Glenfiddich 12 Year.png",
-      url: "assets/img/whisky_preset/1.png",
-    },
-    avg_rating: 3.36,
-    review_count: 32,
-    category: "Single Malt",
-    location: "Speyside, Scotland",
-    priceTier: 2,
-    abv: 40,
-    caskType: "American oak ex-bourbon, European oak ex-sherry",
-    flavor: {
-      smoky: 20,
-      peaty: 10,
-      spicy: 40,
-      herbal: 30,
-      oily: 30,
-      body: 60,
-      rich: 60,
-      sweet: 60,
-      salty: 20,
-      vanilla: 70,
-      tart: 50,
-      fruity: 70,
-      floral: 50,
-    },
-  };
-
   return (
     <>
-      <SContainer>
-        <STitleDiv>
-          <STitleP>리뷰 작성</STitleP>
-          <SP>아래 위스키에 대한 리뷰를 작성해주세요</SP>
-        </STitleDiv>
-        <ReviewDetailInfo whisky={exampleWhisky} />
-        <SContentDiv>
-          <SContentP>사진 등록(최대 5장)</SContentP>
-        </SContentDiv>
-        <ImageUploader images={images} setImages={setImages} />
-        <SContentDiv>
-          <SContentP>리뷰 내용</SContentP>
-        </SContentDiv>
-        <ReviewContent content={content} setContent={setContent} />
-        <SContentDiv>
-          <SContentP>평점 등록</SContentP>
-        </SContentDiv>
-        <ReviewRating rating={rating} setRating={setRating} />
-        <SButton>작성완료</SButton>
-      </SContainer>
+      <form encType="multipart/form-data">
+        <SContainer>
+          <STitleDiv>
+            <STitleP>리뷰 작성</STitleP>
+            <SP>아래 위스키에 대한 리뷰를 작성해주세요</SP>
+          </STitleDiv>
+          <ReviewDetailInfo whisky={whisky} />
+          <SContentDiv>
+            <SContentP>사진 등록(최대 5장)</SContentP>
+          </SContentDiv>
+          <ImageUploader
+            preImages={preImages}
+            handlePreImages={handlePreImages}
+            handleFiles={handleFiles}
+            files={files}
+            maxNum={5}
+          />
+          <SContentDiv>
+            <SContentP>리뷰 내용</SContentP>
+          </SContentDiv>
+          <ReviewContent content={content} setContent={setContent} />
+          <SContentDiv style={{ justifyContent: "center" }}>
+            <SContentP>평점 등록</SContentP>
+          </SContentDiv>
+          <ReviewRating rating={rating} setRating={setRating} />
+          <SButton type="submit" onClick={reviewSave}>
+            작성 완료
+          </SButton>
+        </SContainer>
+      </form>
     </>
   );
 };
