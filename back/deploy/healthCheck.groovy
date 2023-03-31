a165dead69e480f0b28ee1e2809c2c0c08abf145
이다운 <innocent61@naver.com>
2023-03-31 16:42:55 +0900

    :sparkles: Feat: 여러 대의 서버 상태 점검을 수행할 수 있도록 수정
    Former-commit-id: 11b5bc41a54c973d280a428e63d3084c416281e4
    Former-commit-id: 5079c47e23dc7de6b42068794cab170eab36d9db

--- back/deploy/healthCheck.groovy
+++ back/deploy/healthCheck.groovy
@@ -1,17 +1,19 @@
 #!/usr/bin/env groovy
 
-def call(maxAttemps = 5, sleepTime = 10) {
-    def checkCommnad = "curl -f https://j8a805.p.ssafy.io/api/health"
+def call(List<String> ports, int maxAttempts = 5, int sleepTime = 10) {
+    ports.each { port ->
+        def checkCommand = "curl -f http://j8a805.p.ssafy.io:${port}/api/health" as Object
 
-    while (maxAttemps-- > 0) {
-        try {
-            sh(checkCommnad)
-            return
-        } catch (Exception e) {
-            println("서버 감지 실패, ${sleepTime} 후 재확인 하겠습니다.")
-            sleep sleepTime
+        while (maxAttempts-- > 0) {
+            try {
+                sh(checkCommand)
+                return
+            } catch (Exception e) {
+                println("${port}를 사용하는 서버 감지 실패, ${sleepTime} 후 재확인 하겠습니다.")
+                sleep sleepTime
+            }
         }
     }
 
-    error("새로운 스프링 서버 컨테이너의 실행에 실패했습니다.")
+    error("모든 서버에서 실행에 실패했습니다.")
 }
