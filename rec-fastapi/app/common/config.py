79cd5caf19315a8dde50fb13a8021fdebd414356
JunHyxxn <ihrug000@gmail.com>
2023-04-03 17:22:25 +0900

    :sparkles: Feat: env 에 따라서 log level 설정 추가, rating csv 경로 추가
    Former-commit-id: d54bf49b7ed897d5ad120f9ef9061f41987f3416
    Former-commit-id: c50b9f034c8782601ec77850189b18e029aa6d8d

--- rec-fastapi/app/common/config.py
+++ rec-fastapi/app/common/config.py
@@ -1,7 +1,9 @@
 from typing import List, Union
+import logging
 
 from pydantic import AnyHttpUrl, BaseSettings, validator
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
@@ -13,8 +15,11 @@ class Settings(BaseSettings):
     DATASET_PATH: str
     ITEM_FEATURES_PATH: str
     USER_FEATURES_PATH: str
+    RATING_PATH: str
     ENCODING: str
     N_USERS: int
+    LOG_LEVEL: str
+    ENV: str
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
@@ -29,4 +34,29 @@ class Settings(BaseSettings):
         env_file = ".env"
 
 
-settings = Settings()
+class DevSettings(Settings):
+    class Config:
+        env_file = ".env.dev"
+
+
+class ProdSettings(Settings):
+    class Config:
+        env_file = ".env.prod"
+
+
+def get_settings() -> Union[DevSettings, ProdSettings]:
+    if os.getenv("ENV") == "PROD":
+        return ProdSettings()
+    return DevSettings()
+
+
+settings = get_settings()
+
+
+logging.basicConfig(
+    level=settings.LOG_LEVEL,  # 로그 수준
+    format="%(asctime)s %(levelname)s %(message)s",
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
