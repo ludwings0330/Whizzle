74a7123e5cef36ac96ab07e7ce18098852dc1a8b
JunHyxxn <ihrug000@gmail.com>
2023-04-04 22:18:51 +0900

    :sparkles: Feat: 신규 사용자 파일 결합, 모델 재학습, 정확도 평가 로직 구현
    Former-commit-id: 1e9bed4575dd8f1712045d0312bf80b9933f0d73
    Former-commit-id: 635de2b0f8495dd149efa93ae92aeb2f3e1a2f53

--- rec-fastapi/app/routers/retrain.py
+++ rec-fastapi/app/routers/retrain.py
@@ -3,7 +3,7 @@ import logging
 
 from models.dto.data_class import MemberData
 from common.context.ItemFeatures import ItemFeatures
-from service.retrain_service import fit_partial_user
+from service.retrain_service import fit_partial_user, refitting
 
 
 rec = APIRouter(
@@ -24,9 +24,18 @@ async def retrain_exist_user(
     )
     return
 
-    # @rec.post("/retrain/new", status_code=200)
-    # async def retrain_new_model(
-    #     whisky_id: int = Path(..., ge=1),
-    #     item_features: ItemFeatures = Depends(ItemFeatures),
-    # ):
-    #     # return predict_similar_whisky(whisky_id, item_features.data)
+
+@rec.post("/retrain/new", status_code=202)
+async def retrain_new_model(
+    background_tasks: BackgroundTasks,
+    memberData: MemberData = Body(..., alias="memberData"),
+    item_features: ItemFeatures = Depends(ItemFeatures),
+):
+    logging.debug("신규 사용자 추가 학습 : {}".format(memberData))
+    background_tasks.add_task(refitting_model, memberData, item_features.data)
+    return
+
+
+def refitting_model(memberData: MemberData, item_features: ItemFeatures):
+    refitting(memberData.ratings, memberData.preferences, item_features)
+    pass
