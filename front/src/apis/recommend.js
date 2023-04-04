27bee6ac9be9a1097e2a8d1687f9b9099aed5db4
ludwings0330 <ludwings0330@gmail.com>
2023-04-04 14:15:00 +0900

    :hammer: Refactor: 불필요 로그, 주석 삭제
    Former-commit-id: 5c0da9048f7a6fcc13d148f07229c877218dbe9a
    Former-commit-id: 1595251c198380ec6c0ac0d535cd66fd3b871da6

--- front/src/apis/recommend.js
+++ front/src/apis/recommend.js
@@ -3,10 +3,8 @@ import api from "./axiosInstance";
 export const preferenceSave = async (data) => {
   try {
     const res = await api.post(`/api/members/preference`, data);
-    console.log("취향 정보 저장 성공");
     return true;
   } catch (error) {
-    console.log("취향 정보 저장 실패");
     return false;
   }
 };
@@ -14,10 +12,8 @@ export const preferenceSave = async (data) => {
 export const recommend = async (data) => {
   try {
     const res = await api.post(`/api/rec/whisky/any`, data);
-    console.log("추천 결과 성공");
     return res.data;
   } catch (error) {
-    console.log("추천 결과 실패");
     console.log(error);
   }
 };
