9da214007535bef920f78f03685f6c06ff7c1712
JunHyxxn <ihrug000@gmail.com>
2023-04-05 17:59:42 +0900

    :memo: Comment: 버그 잡기 위해 주석 남기기
    Former-commit-id: cad9520298074570c10fb60e0fef4f33679033e8
    Former-commit-id: b4293432269ac100819febd8785eed1f4b30114a

--- back/src/main/java/com/bear/whizzle/retrain/controller/RetrainController.java
+++ back/src/main/java/com/bear/whizzle/retrain/controller/RetrainController.java
@@ -3,6 +3,7 @@ package com.bear.whizzle.retrain.controller;
 import com.bear.whizzle.retrain.controller.dto.SavedModelRequestDto;
 import com.bear.whizzle.retrain.service.RetrainService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class RetrainController {
 
     private final RetrainService retrainService;
@@ -25,6 +27,7 @@ public class RetrainController {
     public void insertSaveeModelData(
             @RequestBody SavedModelRequestDto savedModelRequestDto
     ) {
+        log.info("학습된 모델 저장 정보 : {}", savedModelRequestDto);
         retrainService.saveRetrainModel(savedModelRequestDto);
     }
 
