b5198af29478081ff142cee59d2fca2d9bec4380
zxzx9404 <zxzx9404@gmail.com>
2023-04-05 10:21:03 +0900

    :bug: Fix: 라이브 테스트 문제 수정
    Former-commit-id: 9b658a7fc7f593144a06495850c80318adf9315f
    Former-commit-id: cb4d5d90b0dd76dfc3001522ef7d25c6f5eb657b

--- front/src/hooks/Logout.js
+++ front/src/hooks/Logout.js
@@ -1,8 +1,11 @@
 import { useSetRecoilState } from "recoil";
 import { userState } from "../store/userStore";
+import { preference, recommendResult } from "../store/indexStore";
 
 function Logout() {
   const setUser = useSetRecoilState(userState);
+  const setPref = useSetRecoilState(preference);
+  const setRecResult = useSetRecoilState(recommendResult);
 
   const logout = () => {
     // 토큰 삭제
@@ -21,6 +24,31 @@ function Logout() {
       },
       level: 0,
     });
+
+    setPref({
+      gender: "",
+      age: "",
+      price: "",
+      isExperience: "",
+      whiskies: [],
+      flavor: {
+        smoky: 0,
+        peaty: 0,
+        spicy: 0,
+        herbal: 0,
+        oily: 0,
+        body: 0,
+        rich: 0,
+        sweet: 0,
+        salty: 0,
+        vanilla: 0,
+        tart: 0,
+        fruity: 0,
+        floral: 0,
+      },
+    });
+
+    setRecResult([]);
   };
 
   return logout;
