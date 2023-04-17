9d1c7ea1b50533df372ccc4e0e591df7e113f266
ludwings <81959681+ludwings0330@users.noreply.github.com>
2023-04-17 09:18:32 +0900

    Update constants.js

--- front/src/constants/constants.js
+++ front/src/constants/constants.js
@@ -1,6 +1,4 @@
 export const BASE_URL = "https://whizzle.o-r.kr"; // 배포용
-// export const BASE_URL = "http://j8a805.p.ssafy.io:8080"; // 테스트용
-// export const BASE_URL = "http://localhost:8080"; // 로컬 테스트용
 
 export const LOCAL_FRONT_URL = "http://localhost:3000"; // 로컬 프론트
 
